89c5f33dd7930cb0fd5cf6bc7508f07e3fbab6d3
DabbacheDjawad <djawadmne@gmail.com>
2024-10-07 01:09:54 +0200

    listrace completion

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const revName = document.getElementById("reviewer-name");
 const revImg = document.getElementById("reviewer-img");
 const header = document.querySelector(".header");
+const navItems = document.querySelector("#nav-links");
+const responsiveBtn = document.querySelector("#responsive-btn");
 const numbers = document.querySelectorAll(".nb");
 
 
+//object of reviewers  used in change reviewer function
 const reviewers = [
     {
         name:"Tom Leaker",
@@ -26,6 +29,8 @@ const reviewers = [
     }
 ];
 
+
+//change reviewer function 
 let counter = 0;
 function changeReviewer(){
     if(counter>3){
@@ -35,16 +40,19 @@ function changeReviewer(){
     revImg.src = item.img;
     revName.textContent = item.name;
 }
-
+//we use set interval to change the reviewer every 3seconds
 setInterval(()=>{
     changeReviewer();
     counter++;
 },3000)
 
+
+//sticky navbar function
   window.onscroll = function() {
    
     var sticky = header.offsetTop;
 
+    //scrollY gives you how many pixels you scrolled on the Y axis (vertically)
     if (window.scrollY > sticky) {
       header.classList.add("sticky");
     } else {
@@ -52,11 +60,17 @@ setInterval(()=>{
     }
   };
 
+
+
+
+  //part of the incr function
 function getRandomPosition(){
     return Math.floor(Math.random()*100)+50;
 }
 
 
+
+//incrementing numbers function
 function incr(){
     for(let i = 0;i<numbers.length;i++){
         const a = Number(numbers[i].textContent);
@@ -68,4 +82,19 @@ function incr(){
         },100);
     }
 }
+
+
+//responsive navbar function
+responsiveBtn.addEventListener("click" , ()=>{
+    if(navItems.classList.contains("active")){
+        navItems.classList.remove("active");
+        
+    }else{
+        navItems.classList.add("active");
+    }
+    console.log(navItems.classList);
+    
+})
+
+
 incr();
